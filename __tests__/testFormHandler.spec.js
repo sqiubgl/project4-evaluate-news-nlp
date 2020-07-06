2f670b795e5f30d86ab612e777dd35af6987a383
BGLCROP/sqiu <sqiu@bglcorp.com.au>
2020-07-06 18:48:45 +1000

    Added Post Method to test

--- __tests__/testFormHandler.spec.js
+++ __tests__/testFormHandler.spec.js
@@ -1,7 +1,14 @@
 import { handleSubmit } from "../src/client/js/formHandler";
 
 describe("Testing the submit functionality", () => {
-  test("Testing the handleSubmit() function", () => {
+  test("Test if handleSubmit() is defined", () => {
     expect(handleSubmit).toBeDefined();
+  });
+  test("Test if post is working", async (done) => {
+    const data = await handleSubmit("http://localhost:8081/api", {
+      text: "ABC Test",
+    });
+    expect(data.text).toBe("ABC Test");
+    done();
   });
 });
